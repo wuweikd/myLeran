7db52edf6ac0e4bae0c05cc4512cef64fca1c816
wuweikd <875249771@qq.com>
2020-05-05 15:32:08 +0800

    feat: prettier 添加

--- packages/wuwei-1/src/index.ts
+++ packages/wuwei-1/src/index.ts
@@ -1,7 +1,8 @@
 class Index {
   constructor() {
-    console.log("test eslint");
-    let a = 1
-    console.log('a1', a)
+    console.log('test eslint');
+    let a=1 // eslint 不生效？
+    // a = '111'
+    console.log('a', a)
   }
 }
